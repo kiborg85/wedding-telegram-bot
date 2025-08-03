3bc13e47eb127f3c044cd409c85f5c12da3ab850
Korzhov Mihail <67170016+kiborg85@users.noreply.github.com>
2025-08-03 17:53:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,23 @@
+import time
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from config import BOT_TOKEN, CORRECT_NUMBER  # импорт из отдельного файла
-
 def start(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        "👋 Привет! Чтобы получить следующую подсказку, введи номер нашего свидетельства про одруження 💍"
+    chat_id = update.effective_chat.id
+
+    # Первое сообщение — романтичное
+    context.bot.send_message(
+        chat_id=chat_id,
+        text="💖 Ты — самая красивая, умная и замечательная женщина на свете.\n\nИ это квест только для тебя 🌟"
+    )
+
+    # Подождать 5 секунд
+    time.sleep(5)
+
+    # Второе сообщение — инструкция
+    context.bot.send_message(
+        chat_id=chat_id,
+        text="🗝️ Чтобы пройти к следующему этапу, тебе нужно ввести номер самого важного документа в нашей жизни — свидетельства про одруження 💍"
     )
 
 def check_number(update: Update, context: CallbackContext):
