9db5d3109b900f3c200e6b77125f97b15440438b
Korzhov Mihail <67170016+kiborg85@users.noreply.github.com>
2025-08-04 13:26:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 # config.py
 BOT_TOKEN = "123456789:your_real_token"
-CORRECT_NUMBER = "123456"
 OPENAI_API_KEY = "sk-proj..."
+# Список правильных ответов
+VALID_ANSWER_RAW_LIST = [
+    "111",
+    "568429",
+    "5-мо 568429",
+    "5мо568429"
+]
