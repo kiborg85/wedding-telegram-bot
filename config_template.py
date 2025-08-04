76aec277078d9166bb0f3a898acd4007d90fe844
Korzhov Mihail <67170016+kiborg85@users.noreply.github.com>
2025-08-04 13:38:34 +0300

    Update config_template.py

--- config_template.py
+++ config_template.py
@@ -1,1 +1,9 @@
- 
+BOT_TOKEN = "123456789:your_real_token"
+OPENAI_API_KEY = "sk-proj..."
+# Список правильных ответов
+VALID_ANSWER_RAW_LIST = [
+    "111",
+    "568429",
+    "5-мо 568429",
+    "5мо568429" 
+]
